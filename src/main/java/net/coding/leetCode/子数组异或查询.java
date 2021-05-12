9d52125d439ca000d5d2ef312b41bd42fe05387c
foolmouse <985440944@qq.com>
2021-05-12 22:48:42 +0800

    子数组异或查询

---
+++ src/main/java/net/coding/leetCode/子数组异或查询.java
@@ -0,0 +1,74 @@
+package net.coding.leetCode;
+
+/**
+ * 有一个正整数数组 arr，现给你一个对应的查询数组 queries，其中 queries[i] = [Li, Ri]。
+ * <p>
+ * 对于每个查询 i，请你计算从 Li 到 Ri 的 XOR 值（即 arr[Li] xor arr[Li+1] xor ... xor arr[Ri]）作为本次查询的结果。
+ * <p>
+ * 并返回一个包含给定查询 queries 所有结果的数组。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：arr = [1,3,4,8], queries = [[0,1],[1,2],[0,3],[3,3]]
+ * 输出：[2,7,14,8]
+ * 解释：
+ * 数组中元素的二进制表示形式是：
+ * 1 = 0001
+ * 3 = 0011
+ * 4 = 0100
+ * 8 = 1000
+ * 查询的 XOR 值为：
+ * [0,1] = 1 xor 3 = 2
+ * [1,2] = 3 xor 4 = 7
+ * [0,3] = 1 xor 3 xor 4 xor 8 = 14
+ * [3,3] = 8
+ * 示例 2：
+ * <p>
+ * 输入：arr = [4,8,2,10], queries = [[2,3],[1,3],[0,0],[0,3]]
+ * 输出：[8,0,4,4]
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= arr.length <= 3 * 10^4
+ * 1 <= arr[i] <= 10^9
+ * 1 <= queries.length <= 3 * 10^4
+ * queries[i].length == 2
+ * 0 <= queries[i][0] <= queries[i][1] < arr.length
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/xor-queries-of-a-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 子数组异或查询 {
+
+    /**
+     * 算了懒得写，理解题目比解题过程还麻烦
+     * @param arr
+     * @param queries
+     * @return
+     */
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int[] result = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int[] tem = queries[i];
+            int start = tem[0];
+            int end = tem[1];
+
+            int base = 0;
+            for (int j = start; j <= end; j++) {
+                base = base ^ arr[j];
+            }
+            result[i] = base;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(1^3);
+    }
+
+}
